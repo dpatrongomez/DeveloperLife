2f88816708444ca9fb8b4f5102fe92ec073a6331
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-03-19 07:50:58 +0100

    Initial commit

---
+++ app/src/main/java/es/dpatrongomez/developerlife/MainActivity.java
@@ -0,0 +1,49 @@
+package es.dpatrongomez.developerlife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.suke.widget.SwitchButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final com.suke.widget.SwitchButton work = findViewById(R.id.work);
+        final com.suke.widget.SwitchButton social_life = findViewById(R.id.social_life);
+        final com.suke.widget.SwitchButton sleep = findViewById(R.id.sleep);
+
+        work.setChecked(true);
+        work.isChecked();
+        work.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                if (sleep.isChecked() && social_life.isChecked()) {
+                    work.setChecked(false);
+                }
+                if (work.isChecked() && sleep.isChecked()) {
+                    social_life.setChecked(false);
+                }
+            }
+        });
+        social_life.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                if (social_life.isChecked() && sleep.isChecked()) {
+                    work.setChecked(false);
+                }
+            }
+        });
+        sleep.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                if (sleep.isChecked() && work.isChecked()) {
+                    social_life.setChecked(false);
+                }
+            }
+        });
+    }
+}
