b9d7b5d9774ed8dbe5d14fce74c4a4a72e45a1fd
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2019-02-20 08:38:34 +0100

    Migrate to AndroidX

--- app/src/main/java/es/dpatrongomez/developerlife/MainActivity.java
+++ app/src/main/java/es/dpatrongomez/developerlife/MainActivity.java
@@ -1,6 +1,6 @@
 package es.dpatrongomez.developerlife;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.suke.widget.SwitchButton;
